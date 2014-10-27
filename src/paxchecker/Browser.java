627bd1da4571eecab73d097be1fbd2a328808dc0
Sean Halloran <seanjhalloran@gmail.com>
2014-10-27 02:00:43 -0400

    organizes imports

--- src/paxchecker/Browser.java
+++ src/paxchecker/Browser.java
@@ -1,8 +1,16 @@
 package paxchecker;
 
 import java.awt.Desktop;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.UnknownHostException;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
--- src/paxchecker/Email.java
+++ src/paxchecker/Email.java
@@ -1,7 +1,15 @@
 package paxchecker;
 
-import java.util.*;
-import javax.mail.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
